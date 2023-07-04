9b8247e17628b18a3bb2b9c99780b41fbf19a50f
George Rusu <58792068+georgerusu017@users.noreply.github.com>
2023-07-04 13:42:55 +0300

    undo fixes

--- src/models/History.model.tsx
+++ src/models/History.model.tsx
@@ -1,4 +1,4 @@
-export interface CellHistory {
+export interface CellHistoryItem {
     value: string,
     noteValues: string[],
     index: number
--- src/pages/SudokuGame.tsx
+++ src/pages/SudokuGame.tsx
@@ -1,218 +1,191 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import TimeControl from '../components/TimeControl/TimeControlFirstTry';
-import GameBoard from '../components/GameBoard/GameBoard';
-import ControlBoard from '../components/ControlBoard/ControlBoard';
-import "../pages/SudokuGame.css"
-import { Cell } from '../models/Cell.model';
-import { generateSudoku } from '../services/sudoku.service';
-import { calculateSelectedCellNewPosition, decreaseInvalidCount, highlightCells, increaseInvalidCount } from '../services/cellManipulation.service';
-import { CellHistory } from '../models/History.model';
+import React, { useCallback, useEffect, useState } from "react";
+import TimeControl from "../components/TimeControl/TimeControlFirstTry";
+import GameBoard from "../components/GameBoard/GameBoard";
+import ControlBoard from "../components/ControlBoard/ControlBoard";
+import "../pages/SudokuGame.css";
+import { Cell } from "../models/Cell.model";
+import { generateSudoku } from "../services/sudoku.service";
+import {
+  calculateSelectedCellNewPosition,
+  decreaseInvalidCount,
+  highlightCells,
+  increaseInvalidCount,
+} from "../services/cellManipulation.service";
+import { CellHistoryItem } from "../models/History.model";
 
 export default function SudokuGame() {
+  const [cells, setCells] = useState<Cell[]>(generateSudoku);
+  const [selectedCell, setSelectedCell] = useState<Cell>(cells[0]);
+  const [notesToggle, setNotesToggle] = useState<boolean>(false);
+  const [history, setHistory] = useState<CellHistoryItem[]>([]);
 
-   const [cells, setCells] = useState<Cell[]>(generateSudoku)
-   const [selectedCell, setSelectedCell] = useState<Cell>(cells[0])
-   const [notesToggle, setNotesToggle] = useState<boolean>(false)
-   const [history, setHistory] = useState<CellHistory[]>([])
+  function resumeTimer() {
+    console.log("test");
+  }
 
-   function resumeTimer() {
-      console.log("test")
-   }
+  const addToHistory = useCallback(() => {
+    if (!selectedCell.isEditable) {
+      return;
+    }
+    const cellHistoryItem = {
+      value: selectedCell.value,
+      noteValues: [...selectedCell.noteValues],
+      index: cells.indexOf(selectedCell),
+    };
+    const newHistory = [...history, cellHistoryItem];
+    setHistory(newHistory);
+  }, [cells, history, selectedCell]);
 
-   const addToHistory = useCallback(() => {
-      const cellDataToHistory = {
-         value: selectedCell.value, noteValues: [...selectedCell.noteValues], index: cells.indexOf(selectedCell)
-      }
-      const newHistory = [...history, cellDataToHistory]
-      setHistory(newHistory)
-   },[cells, history, selectedCell])      
-
-   const handleSelectedCell = useCallback((cell: Cell) => {
-
-      const newCells = [...cells]
-
-      highlightCells(cell, newCells)
-      setSelectedCell(cell)
-      setCells(newCells)
-
-   }, [cells])
-
-   const handleNewGame = useCallback(() => {
-      const newCells = generateSudoku()
-      highlightCells(newCells[0], newCells);
-      setNotesToggle(false);
-
-      setCells(newCells);
-      setSelectedCell(newCells[0]);
-   }, [])
-
-   useEffect(() => {
-      handleSelectedCell(cells[0])
-   }, [])
-
-   const handleArrowKeyPress = useCallback((key: string) => {
-
-      const newId = calculateSelectedCellNewPosition(selectedCell, key)
-      const newSelectedCell = cells.find(cell => cell.id === newId)
-
-      if (newSelectedCell) {
-         handleSelectedCell(newSelectedCell)
-      }
-
-   }, [selectedCell, cells, handleSelectedCell])
-
-   const handleValueChange = useCallback((value: string) => {
-
+  const handleSelectedCell = useCallback(
+    (cell: Cell) => {
       const newCells = [...cells];
 
-      addToHistory()
+      highlightCells(cell, newCells);
+      setSelectedCell(cell);
+      setCells(newCells);
+    },
+    [cells]
+  );
+
+  const handleNewGame = useCallback(() => {
+    const newCells = generateSudoku();
+    highlightCells(newCells[0], newCells);
+    setNotesToggle(false);
+
+    setCells(newCells);
+    setSelectedCell(newCells[0]);
+  }, []);
+
+  useEffect(() => {
+    handleSelectedCell(cells[0]);
+  }, []);
+
+  const handleArrowKeyPress = useCallback(
+    (key: string) => {
+      const newId = calculateSelectedCellNewPosition(selectedCell, key);
+      const newSelectedCell = cells.find((cell) => cell.id === newId);
+
+      if (newSelectedCell) {
+        handleSelectedCell(newSelectedCell);
+      }
+    },
+    [selectedCell, cells, handleSelectedCell]
+  );
+
+  const handleValueChange = useCallback(
+    (value: string) => {
+      const newCells = [...cells];
+
+      addToHistory();
 
       if (!selectedCell.isEditable) {
-         return
+        return;
       }
 
       if (!notesToggle) {
+        selectedCell.noteValues.length = 0;
 
-         selectedCell.noteValues.length = 0;
-
-         if (selectedCell.value === '') {
-
-            selectedCell.value = value;
-            increaseInvalidCount(selectedCell, cells)
-
-         }
-         else if (selectedCell.value === value) {
-
-            decreaseInvalidCount(selectedCell, cells)
-            selectedCell.value = '';
-
-         }
-         else if (selectedCell.value !== value) {
-
-            decreaseInvalidCount(selectedCell, cells)
-            selectedCell.value = value;
-            increaseInvalidCount(selectedCell, cells)
-
-         }
-      }
-
-      else {
-
-         const notesIndex = Number(value) - 1;
-
-         if (selectedCell.value !== '') {
-
-            decreaseInvalidCount(selectedCell, cells)
-            selectedCell.value = '';
-
-         }
-
-         if (selectedCell.noteValues[notesIndex] === value) {
-            selectedCell.noteValues[notesIndex] = '';
-         }
-         else selectedCell.noteValues[notesIndex] = value;
-
-
-      }
-
-      highlightCells(selectedCell, newCells)
-      setCells(newCells)
-
-   }, [addToHistory, cells, notesToggle, selectedCell])
-
-   const handleNotesDelete = useCallback(() => {
-
-      if (selectedCell.noteValues.length > 0) {
-         selectedCell.noteValues.length = 0;
-      }
-
-   }, [selectedCell.noteValues])
-
-   const handleDelete = useCallback(() => {
-
-      addToHistory()
-      handleValueChange('')
-      handleNotesDelete()
-
-   }, [addToHistory, handleNotesDelete, handleValueChange])
-
-   const handleUndo = useCallback(() => {
-
-      const newCells = [...cells]
-      const lastCell = history.pop()
-
-      if (lastCell) {
-
-         decreaseInvalidCount(newCells[lastCell.index], cells)
-         newCells[lastCell.index].value = '';
-         setCells(newCells)
-         
-         handleNotesDelete()
-
-         if (lastCell.value) {
-            newCells[lastCell.index].value = lastCell.value
-            increaseInvalidCount(newCells[lastCell.index], cells)
-         }
-
-         else {
-            newCells[lastCell.index].noteValues = lastCell.noteValues
-         }
-
-         setSelectedCell(newCells[lastCell.index])
-         setCells(newCells)
-
-      }
-
-      console.log('hit');
-
-   }, [cells, handleNotesDelete, history])
-
-   const handleKeyDown = useCallback((event: KeyboardEvent) => {
-
-      if (/^[1-9]$/.test(event.key)) {
-         handleValueChange(event.key)
-      }
-      else if (event.key === 'Delete') {
-         handleDelete()
+        if (selectedCell.value === "") {
+          selectedCell.value = value;
+          increaseInvalidCount(selectedCell, cells);
+        } else if (selectedCell.value === value) {
+          decreaseInvalidCount(selectedCell, cells);
+          selectedCell.value = "";
+        } else if (selectedCell.value !== value) {
+          decreaseInvalidCount(selectedCell, cells);
+          selectedCell.value = value;
+          increaseInvalidCount(selectedCell, cells);
+        }
       } else {
-         handleArrowKeyPress(event.key)
+        const notesIndex = Number(value) - 1;
+
+        if (selectedCell.value !== "") {
+          decreaseInvalidCount(selectedCell, cells);
+          selectedCell.value = "";
+        }
+
+        if (selectedCell.noteValues[notesIndex] === value) {
+          selectedCell.noteValues[notesIndex] = "";
+        } else selectedCell.noteValues[notesIndex] = value;
       }
 
-   }, [handleArrowKeyPress, handleDelete, handleValueChange])
+      highlightCells(selectedCell, newCells);
+      setCells(newCells);
+    },
+    [addToHistory, cells, notesToggle, selectedCell]
+  );
 
-   useEffect(() => {
-      document.addEventListener("keydown", handleKeyDown)
+  const handleNotesDelete = useCallback(() => {
+    if (selectedCell.noteValues.length > 0) {
+      selectedCell.noteValues.length = 0;
+    }
+  }, [selectedCell.noteValues]);
 
-      return () => {
-         document.removeEventListener("keydown", handleKeyDown)
+  const handleDelete = useCallback(() => {
+    handleValueChange("");
+    handleNotesDelete();
+  }, [handleNotesDelete, handleValueChange]);
+
+  const handleUndo = useCallback(() => {
+    const newCells = [...cells];
+    const lastCell = history.pop();
+
+    if (!lastCell) {
+      return;
+    }
+
+    decreaseInvalidCount(newCells[lastCell.index], cells);
+    newCells[lastCell.index].value = lastCell.value;
+    newCells[lastCell.index].noteValues = lastCell.noteValues;
+
+    if (lastCell.value) {
+      increaseInvalidCount(newCells[lastCell.index], cells);
+    }
+
+    highlightCells(newCells[lastCell.index], newCells);
+    setSelectedCell(newCells[lastCell.index]);
+    setCells(newCells);
+  }, [cells, history]);
+
+  const handleKeyDown = useCallback(
+    (event: KeyboardEvent) => {
+      if (/^[1-9]$/.test(event.key)) {
+        handleValueChange(event.key);
+      } else if (event.key === "Delete") {
+        handleDelete();
+      } else {
+        handleArrowKeyPress(event.key);
       }
+    },
+    [handleArrowKeyPress, handleDelete, handleValueChange]
+  );
 
-   }, [handleKeyDown])
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
 
-   const handleNotesToggle = useCallback(() => {
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleKeyDown]);
 
-      setNotesToggle((notesToggle) => !notesToggle)
-      console.log(notesToggle);
+  const handleNotesToggle = useCallback(() => {
+    setNotesToggle((notesToggle) => !notesToggle);
+  }, []);
 
-   }, [notesToggle])
-
-   return (
-      <div className="sudoku-game">
-         <div className="timer-game">
-            <TimeControl onResume={resumeTimer} />
-            <GameBoard
-               cells={cells}
-               onSelectCell={handleSelectedCell} />
-         </div>
-         <ControlBoard
-            onNewGameClick={handleNewGame}
-            onNumberButtonClick={handleValueChange}
-            onUndoClick={handleUndo}
-            onDeleteClick={handleDelete}
-            onNotesClick={handleNotesToggle}
-            notesToggle={notesToggle}
-         />
+  return (
+    <div className="sudoku-game">
+      <div className="timer-game">
+        <TimeControl onResume={resumeTimer} />
+        <GameBoard cells={cells} onSelectCell={handleSelectedCell} />
       </div>
-   )
+      <ControlBoard
+        onNewGameClick={handleNewGame}
+        onNumberButtonClick={handleValueChange}
+        onUndoClick={handleUndo}
+        onDeleteClick={handleDelete}
+        onNotesClick={handleNotesToggle}
+        notesToggle={notesToggle}
+      />
+    </div>
+  );
 }
-
